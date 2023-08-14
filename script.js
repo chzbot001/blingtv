7af1a1340e901be64994d8ea530dec070340da5c
chzbot001 <44459445+chzbot001@users.noreply.github.com>
2023-08-14 15:13:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         We-play
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @require      https://raw.githubusercontent.com/chzbot001/blingtv/main/main.js
+// @updateURL    https://raw.githubusercontent.com/chzbot001/blingtv/main/script.js
+// @downloadURL  https://raw.githubusercontent.com/chzbot001/blingtv/main/script.js
+// @match        https://blingtv.net/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=blingtv.net
+// @grant        none
+// ==/UserScript==
