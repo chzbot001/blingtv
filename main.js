424669731ac21c36f156b6224e7959ca35f34811
chzbot001 <44459445+chzbot001@users.noreply.github.com>
2023-08-20 19:38:43 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
               setTimeout(function(){
                   $("#ad").show();
                   console.log("show");
-              }, 242000);//242000
+              }, 245000);//242000
   const video = document.getElementById('video');
   const ui = video.ui;
   const controls = ui.getControls();
