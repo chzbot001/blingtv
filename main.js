efff50283df7edf1b1a8204265c30b63991906f6
chzbot001 <44459445+chzbot001@users.noreply.github.com>
2023-08-27 09:52:51 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 (function() {
     'use strict';
+
+    function show(){ 
+        $("#ad").show();
+    }
+    setTimeout(function(){ 
+        $("<button id=\"\" onclick=\"show();\">Show Player</button>").insertBefore("body");
+    }, 10000);
+
+    
   const video = document.getElementById('video');
   const ui = video.ui;
   const controls = ui.getControls();
   const player = controls.getPlayer();
   const config = {'controlPanelElements': ['play_pause','mute','volume','quality','language','overflow_menu','fullscreen']};
   ui.configure(config);
-    function show(){ 
-        $("#ad").show();
-    }
-setTimeout(function(){ 
-$("<button id=\"\" onclick=\"show();\">Show Player</button>").insertBefore("body");
-}, 10000);
 
     
 })();
