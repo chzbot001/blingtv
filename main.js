3a3d1b296ae0bedcccdf936e7560e0c974a8991a
chzbot001 <44459445+chzbot001@users.noreply.github.com>
2023-08-27 09:00:06 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,10 +7,23 @@
   const config = {'controlPanelElements': ['play_pause','mute','volume','quality','language','overflow_menu','fullscreen']};
   ui.configure(config);
 
+function onButtonClick() {
+  alert('Button clicked!');
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', onButtonClick);
+
+
+/*
 var widget = document.getElementById("ad");
 widget.innerHTML = '<button id="mahbutton" href="javascript:show()">Show Player</button>';
 var w_btn = document.getElementById("mahbutton");
-    function() {
+
+function(){ 
+       $("#ad").hide();
+}
+    function show() {
         $("#ad").show();
-    }
+    }*/
 })();
