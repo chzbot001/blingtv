cd0507e1f0e81e1192c490efbbc2b7039fa8a2cb
chzbot001 <44459445+chzbot001@users.noreply.github.com>
2023-08-27 08:52:09 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 (function() {
     'use strict';
-              setTimeout(function(){
-                  $("#ad").show();
-                  console.log("show");
-              }, 245000);//242000
   const video = document.getElementById('video');
   const ui = video.ui;
   const controls = ui.getControls();
@@ -11,4 +7,10 @@
   const config = {'controlPanelElements': ['play_pause','mute','volume','quality','language','overflow_menu','fullscreen']};
   ui.configure(config);
 
+var widget = document.getElementById("ad");
+widget.innerHTML = '<button id="mahbutton" href="javascript:show()">Show Player</button>';
+var w_btn = document.getElementById("mahbutton");
+    function() {
+        $("#ad").show();
+    }
 })();
