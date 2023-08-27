1a1b6f423a416db6f802ed22c1002ceab750a91d
chzbot001 <44459445+chzbot001@users.noreply.github.com>
2023-08-27 09:00:41 +0700

    Update main.js

--- main.js
+++ main.js
@@ -11,9 +11,9 @@ function onButtonClick() {
   alert('Button clicked!');
 }
 
-const button = document.querySelector('button');
-button.addEventListener('click', onButtonClick);
-
+const newButton = document.createElement('button');
+newButton.textContent = 'Click me!';
+document.body.appendChild(newButton);
 
 /*
 var widget = document.getElementById("ad");
