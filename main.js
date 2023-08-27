e0c34c069a81c9e659593ed82fb3e8577577771d
chzbot001 <44459445+chzbot001@users.noreply.github.com>
2023-08-27 11:05:22 +0700

    Update main.js

--- main.js
+++ main.js
@@ -6,21 +6,11 @@
     }
 document.addEventListener("load", function(){
     // ....
-      setTimeout(function(){ 
+
         $("<button id=\"\" onclick=\"show();\">Show Player</button>").insertBefore("body");
           document.getElementById("one").style.width = "50%;
-    }, 10000);
+
     
 });
-  
 
-    /*
-  const video = document.getElementById('video');
-  const ui = video.ui;
-  const controls = ui.getControls();
-  const player = controls.getPlayer();
-  const config = {'controlPanelElements': ['play_pause','mute','volume','quality','language','overflow_menu','fullscreen']};
-  ui.configure(config);
-
-    */
 })();
