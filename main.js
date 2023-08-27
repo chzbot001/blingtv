0c8c82d25f7fbec8e5519b98d8089a471587b4aa
chzbot001 <44459445+chzbot001@users.noreply.github.com>
2023-08-27 14:35:39 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
 /*(function() {
     'use strict';
-*/
-/*function showplayer(){
-        document.getElementById("ad").style.display = "block";
-    }*/
-$("#showplayer").on( "click", function() {
-document.getElementById("ad").style.display = "block";
-} );
-    
-    $(document).ready(function(){ 
-        $("<button id=\"showplayer\" onclick=\"showplayer()\">Show Player</button>").insertBefore("div.flex-item");
-        $("<button id=\"showui\" onclick=\"showui()\">Show Player Controls</button>").insertBefore("div.flex-item");
-        document.getElementById("ad").style.width = "60%";
-        
-const video = document.getElementById('video');
-  const ui = video.ui;
-  const controls = ui.getControls();
-  const player = controls.getPlayer();
-  const config = {'controlPanelElements': ['play_pause','mute','volume','quality','language','overflow_menu','fullscreen']};
-  ui.configure(config);
-    });
-/*
-})();*/
+    $(document).ready(function(){
+        $("<button style=\"background-color: #00f6ff;color: black;\" id=\"showplayer\">Show Player (กรณีจอหาย)</button>").insertBefore("#one > center > div > h3");
+        $("#ad").css({ "text-align": "center", "margin-left": "auto", "margin-right": "auto" });
+        $("#showplayer").on( "click", function() {
+            document.getElementById("ad").style.display = "block";
+        } );
+
+        $("#showui").on( "click", function() {
+
+        });
+        setTimeout(function(){
+            const video = document.getElementById('video');
+            const ui = video['ui'];
+            const controls = ui.getControls();
+            const player = controls.getPlayer();
+            const config = {'controlPanelElements': ['play_pause','mute','volume','quality','language','overflow_menu','fullscreen']};
+            ui.configure(config);
+
+        }, 3500);
+        document.getElementById("ad").style.width = "70%";
+        document.getElementById("ustreamIframe").style.height = "700px";
+    } );
+
+})();
