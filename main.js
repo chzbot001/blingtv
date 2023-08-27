cb56057dbec19b1ffc87d5569e9b7c4ed07b4bfc
chzbot001 <44459445+chzbot001@users.noreply.github.com>
2023-08-27 12:01:50 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,24 +7,16 @@
         $("<button id=\"\" onclick=\"show();\">Show Player</button>").insertBefore("div.flex-item");
         $("<button id=\"\" onclick=\"showui();\">Show Player Controls</button>").insertBefore("div.flex-item");
         document.getElementById("ustreamIframe").style.width = "900px";    
-
-      const video = document.getElementById('video');
-      const ui = video.ui;
-      const controls = ui.getControls();
-      const player = controls.getPlayer();
-      const config = {'controlPanelElements': ['play_pause','mute','volume','quality','language','overflow_menu','fullscreen']};
-      ui.configure(config);
-
         
     const show = function (){ 
         $("#ad").show();
     }
     const showui = function (){
-        const video = document.getElementById('video');
+      const video = document.getElementById('video');
       const ui = video.ui;
       const controls = ui.getControls();
       const player = controls.getPlayer();
-      const config = {'controlPanelElements': ['play_pause','mute','volume','quality','language','overflow_menu','fullscreen']};
+      const config = {'controlPanelElements': ['play_pause','volume','quality','overflow_menu','fullscreen']};
       ui.configure(config);
     }
 });
