850b7641a12b1dd791975ddbd45495025ad07dda
chzbot001 <44459445+chzbot001@users.noreply.github.com>
2023-08-27 11:26:01 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-(function() {
+/*(function() {
     'use strict';
-
+*/
     function show(){ 
         $("#ad").show();
     }
-$(document).ready(function(){ 
-
-        $("<button id=\"\" onclick=\"show();\">Show Player</button>").insertBefore("body");
-          document.getElementById("one").style.width = "50%";
-
+    function showui(){
+        const video = document.getElementById('video');
+      const ui = video.ui;
+      const controls = ui.getControls();
+      const player = controls.getPlayer();
+      const config = {'controlPanelElements': ['play_pause','mute','volume','quality','language','overflow_menu','fullscreen']};
+      ui.configure(config);
+    }
     
+    
+    $(document).ready(function(){ 
+        $("<button id=\"\" onclick=\"show();\">Show Player</button>").insertBefore("body");
+        $("<button id=\"\" onclick=\"showui();\">Show Player UI</button>").insertBefore("body");
+        document.getElementById("one").style.width = "75%";    
 });
-
-})();
+/*
+})();*/
