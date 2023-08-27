e82011e5b918629f46e2c884d3922b816d5f2e26
chzbot001 <44459445+chzbot001@users.noreply.github.com>
2023-08-27 14:37:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 // @description  try to take over the world!
 // @author       Supernova
 // @require      https://raw.githubusercontent.com/chzbot001/blingtv/main/main.js
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js
 // @updateURL    https://raw.githubusercontent.com/chzbot001/blingtv/main/script.js
 // @downloadURL  https://raw.githubusercontent.com/chzbot001/blingtv/main/script.js
 // @match        https://blingtv.net/*
