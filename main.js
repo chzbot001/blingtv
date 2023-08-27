8a00bcbbd91a7d8e30cf5764c283097441f6fa90
chzbot001 <44459445+chzbot001@users.noreply.github.com>
2023-08-27 11:51:00 +0700

    Update main.js

--- main.js
+++ main.js
@@ -19,5 +19,11 @@
         $("<button id=\"\" onclick=\"showui();\">Show Player Controls</button>").insertBefore("div.flex-item");
         document.getElementById("ustreamIframe").style.width = "900px";    
 });
+      const video = document.getElementById('video');
+      const ui = video.ui;
+      const controls = ui.getControls();
+      const player = controls.getPlayer();
+      const config = {'controlPanelElements': ['play_pause','mute','volume','quality','language','overflow_menu','fullscreen']};
+      ui.configure(config);
 /*
 })();*/
